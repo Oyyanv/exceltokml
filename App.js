a9b8c4080ac910737fd2456db164adacfb5149a9
Oyyanv <muhammadroyyan527@gmail.com>
2025-01-03 18:02:39 +0700

    pls fix#100

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+//modul express ini untuk jadiin web pake API
 const express = require('express');
+// multer ini gunanya buat unggahan file
 const multer = require('multer');
+// xlsx ini buat baca format xlsx
 const xlsx = require('xlsx');
+// xmlbuilder buat file xmlnya
 const xmlbuilder = require('xmlbuilder');
+// fs itu bawaan dari node.js gunanya itu buat membaca,menulis, dan menghapus sistem file
 const fs = require('fs');
+// path itu juga bawaan dari node.js jadi gunanya itu untuk mendapatkan nama file dari direktori
 const path = require('path');
 
 const app = express();
@@ -12,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Konfigurasi multer untuk upload file
-const upload = multer({ dest: '/tmp/uploads/' });
+const upload = multer({ dest: '/uploads' });
 
 // Fungsi untuk membuat nama file unik
 function generateUniqueFileName(baseName, extension) {
